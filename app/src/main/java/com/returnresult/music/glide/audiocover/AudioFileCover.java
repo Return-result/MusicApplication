a3cde0eb2bb9eb253795254ec79558efa247da46
Patrick-Manyara <pmanyara97@gmail.com>
2021-01-04 16:02:23 +0300

    First commit

---
+++ app/src/main/java/com/returnresult/music/glide/audiocover/AudioFileCover.java
@@ -0,0 +1,9 @@
+package com.returnresult.music.glide.audiocover;
+
+public class AudioFileCover {
+    public final String filePath;
+
+    public AudioFileCover(String filePath) {
+        this.filePath = filePath;
+    }
+}
---
+++ app/src/main/java/com/returnresult/music/interfaces/PaletteColorHolder.java
@@ -0,0 +1,9 @@
+package com.returnresult.music.interfaces;
+
+import androidx.annotation.ColorInt;
+
+public interface PaletteColorHolder {
+
+    @ColorInt
+    int getPaletteColor();
+}
